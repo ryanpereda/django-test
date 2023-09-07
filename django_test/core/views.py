e44007999de2357bc10c98590395bedd43c38ed8
ryanpereda <ryanpereda9001@gmail.com>
2023-09-06 22:12:19 -0400

    added project functionality to the projects page

--- django_test/core/views.py
+++ django_test/core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from item.models import Project
+
 # Create your views here.
 
 def index(request):
@@ -9,4 +11,7 @@ def contact(request):
     return render(request, 'core/contact.html')
 
 def projects(request):
-    return render(request, 'core/projects.html')
+    projects = Project.objects.all()
+    return render(request, 'core/projects.html', {
+        'projects': projects,
+    })
